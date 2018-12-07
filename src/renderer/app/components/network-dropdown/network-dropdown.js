52e7b51ee14564b4ea7c5334f4bc2100461e210a
Justin Barry <328965+justinbarry@users.noreply.github.com>
2018-12-06 16:41:23 -0800

    Sort local and lightnode options to top of dropwdown

--- src/renderer/app/components/network-dropdown/network-dropdown.js
+++ src/renderer/app/components/network-dropdown/network-dropdown.js
@@ -178,7 +178,7 @@ export class NetworkDropdown extends Component {
 		for (let i = 0; i < end.length; i++) {
 	  		const key = end[i]
 	  		const isSelected = (key === selectedKey)
-	  		options.push(
+	  		options.unshift(
 	  			<NetworkDropdownItems
 					key={key}
 					selectNetwork={() => {
