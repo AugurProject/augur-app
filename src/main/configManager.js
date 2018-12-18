699be9c4bbcae0d9c7a8fbeda38bd4e83f9f528c
unknown <achap5dk@gmail.com>
2018-12-17 22:49:32 -0800

    Reconnect display issue. Light node tweaks. Retry AN forever

--- src/main/configManager.js
+++ src/main/configManager.js
@@ -11,19 +11,19 @@ const defaultConfig = {
   'sslPort': '8443',
   'sslEnabled': false,
   'networks': {
-    localLightNode: {
-      'userCreated': false,
-      'http': 'http://127.0.0.1:8545',
-      'name': 'Local Light Node (experimental)',
-      'selected': true,
-      'ws': 'ws://127.0.0.1:8546'
-    },
     mainnet: {
       'userCreated': false,
       'http': 'https://eth-mainnet.alchemyapi.io/jsonrpc/7sE1TzCIRIQA3NJPD5wg7YRiVjhxuWAE',
       'name': 'Mainnet',
       'ws': '',
       'id': '1'
+    },
+    localLightNode: {
+      'userCreated': false,
+      'http': 'http://127.0.0.1:8545',
+      'name': 'Local Light Node (experimental)',
+      'selected': true,
+      'ws': 'ws://127.0.0.1:8546'
     },
     rinkeby: {
       'userCreated': false,
@@ -82,7 +82,7 @@ function ConfigManager() {
 
 ConfigManager.prototype.getSelectedNetwork = function () {
   let selected = Object.values(this.config.networks).find(n => n.selected)
-  if (!selected) selected = Object.values(this.config.networks).find(n => n.name.toLowerCase().indexOf('localLightNode') > -1)
+  if (!selected) selected = Object.values(this.config.networks).find(n => n.name.toLowerCase().indexOf('mainnet') > -1)
   return selected
 }
 
