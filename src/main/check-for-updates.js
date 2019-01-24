e19c1526db66d2d71f77a69678bc5d498e4d4e48
Tom Haile <bradford.haile@gmail.com>
2019-01-24 17:26:06 -0600

    Add db version getters (#458)
    * initial changes for updated db version check
    
    * Update readme with instructions on how to test auto-update locally
    
    * remove property, not needed
    
    * Add .nvmrc file
    
    * added message when full resync is needed
    
    * adding file to pass filename to updater for db version comparision
    
    * puttin db version in file
    
    * revert package.json accidently checked in testing stuff
    
    * remove debug log.errors
    
    * missed a place where check for update was called, needed to pass in user's db version
    
    * removed debugging log outs
    
    * rename file, we need the content keep the filename the same

--- src/main/check-for-updates.js
+++ src/main/check-for-updates.js
@@ -1,15 +1,18 @@
-const {app, dialog, shell } = require('electron')
-const {autoUpdater} = require('electron-updater')
+const { app, dialog, shell } = require('electron')
+const { autoUpdater } = require('electron-updater')
 const ProgressBar = require('electron-progressbar')
 const log = require('electron-log')
-
+const fetch = require('node-fetch')
 const isDev = require('electron-is-dev')
+const dbVersionFilename = 'db_version.txt'
+const githubDownloadUrl = 'https://github.com/AugurProject/augur-app/releases/download/'
+/* global process*/
 
 const downloadAndInstall = () => {
   var progressBar = new ProgressBar({
     text: 'Downloading update',
     detail: '0%',
-    indeterminate: false,
+    indeterminate: false
   })
 
   autoUpdater
@@ -27,35 +30,41 @@ const downloadAndInstall = () => {
 
 const notifyNoUpdate = () => {
   dialog.showMessageBox({
-    message: 'No update is available.',
+    message: 'No update is available.'
   })
 }
 
-const notifyUpdate = (resolve) => (updateInfo) => {
-  dialog.showMessageBox({
-    message: `Update ${updateInfo.version} available. (Current version ${app.getVersion()})`,
-    buttons: [
-      'See Release Notes',
-      'Remind Me Later',
-      'Download and Install Update',
-    ],
-    cancelId: 1,
-    defaultId: 2,
-  }, (data) => {
-    switch (data) {
-      case 2:
-        downloadAndInstall()
-        break
-      case 0:
-        shell.openExternal('https://github.com/AugurProject/augur-app/releases')
-        resolve()
-        break
-      case 1:
-        resolve()
-        break
-      default:
-        resolve()
+const notifyUpdate = (highestDbVersion, resolve) => updateInfo => {
+  let resyncMessage = ''
+  getGithubReleaseFiles(updateInfo.releaseName).then(dbVersionfileContents => {
+    if (parseInt(dbVersionfileContents, 10) > parseInt(highestDbVersion, 10)) {
+      resyncMessage = 'This release will need a full resync.'
     }
+
+    dialog.showMessageBox(
+      {
+        message: `Update ${updateInfo.version} available. ${resyncMessage} (Current version ${app.getVersion()})`,
+        buttons: ['See Release Notes', 'Remind Me Later', 'Download and Install Update'],
+        cancelId: 1,
+        defaultId: 2
+      },
+      data => {
+        switch (data) {
+          case 2:
+            downloadAndInstall()
+            break
+          case 0:
+            shell.openExternal('https://github.com/AugurProject/augur-app/releases')
+            resolve()
+            break
+          case 1:
+            resolve()
+            break
+          default:
+            resolve()
+        }
+      }
+    )
   })
 }
 
@@ -63,26 +72,32 @@ const notifyUpdate = (resolve) => (updateInfo) => {
 autoUpdater.logger = log
 autoUpdater.autoDownload = false
 
-module.exports = (notifyUpdateNotAvailable = false) => {
-  if(isDev) return Promise.resolve()
-  if(process.platform == "linux" && !process.env.APPIMAGE) return Promise.resolve()
+module.exports = (notifyUpdateNotAvailable = false, highestDbVersion = 0) => {
+  if (isDev) return Promise.resolve()
 
-  const p = new Promise((resolve) => {
-    autoUpdater
-      .once('update-not-available', resolve)
-      .once('update-available', notifyUpdate(resolve))
+  if (process.platform == 'linux' && !process.env.APPIMAGE) return Promise.resolve()
+
+  new Promise(resolve => {
+    autoUpdater.once('update-not-available', resolve).once('update-available', notifyUpdate(highestDbVersion, resolve))
   })
 
-  autoUpdater
-    .removeListener('update-not-available', notifyNoUpdate)
+  autoUpdater.removeListener('update-not-available', notifyNoUpdate)
 
-  if (notifyUpdateNotAvailable) autoUpdater
-    .once('update-not-available', notifyNoUpdate)
+  if (notifyUpdateNotAvailable) autoUpdater.once('update-not-available', notifyNoUpdate)
 
+  return autoUpdater.checkForUpdates().catch(e => {
+    log.error('There was an error updating app. This is expected if using deb package.', e)
 
-  return autoUpdater.checkForUpdates().catch((e) => {
-    log.error('There was an error updating app. This is expected if using deb package.')
+    return Promise.resolve()
+  })
+}
 
-    return Promise.resolve();
-  });
+function getGithubReleaseFiles(release) {
+  const url = `${githubDownloadUrl}/${release}/${dbVersionFilename}`
+  return fetch(url)
+    .then(res => res.text())
+    .then(text => {
+      if (text) return text
+      return 0
+    })
 }
