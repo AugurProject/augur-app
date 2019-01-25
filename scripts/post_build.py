609773db21564ff764ef41b1324cd65a21e25e02
Peter Hicks <petong@jah.net>
2019-01-25 14:24:02 -0800

    moves upload_release_asset() it's own draft check

--- scripts/post_build.py
+++ scripts/post_build.py
@@ -112,10 +112,10 @@ for fname in os.listdir(full_path):
             shasums = '{} {}'.format(sha, fname)
             print(shasums)
             shafile.write(shasums)
-        release_id = release_info['id']
-        release = repo.get_release(release_id)
-        if release_info and release.draft:
+        if release_info and release_info['draft'] is True:
+            release_id = release_info['id']
             delete_asset_if_exists(release_info, shasums_file)
+        if release_info['draft'] is True:
             upload_release_asset(release_id, shasums, shasums_file)
 
 upload_database_version(release_id)
