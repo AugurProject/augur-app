23bc7aa1cbbc6106f6827b885538ff8f615b3367
Peter Hicks <petong@jah.net>
2019-03-04 13:19:39 -0800

    adds electorn build commands and sha generation

--- scripts/azure-electron-build.sh
+++ scripts/azure-electron-build.sh
@@ -3,24 +3,29 @@ set -euxo pipefail
 
 env
 
-which python
-which python3
-which pip
-which pip3
 python --version
 
 pip install requests
-pip3 install requests
 
 python -c 'import requests'
 
 if [[ $AGENT_OS == 'Windows_NT' ]]; then
     echo 'Windows'
+    npm run compile && electron-builder --win
 elif [[ $AGENT_OS == 'Darwin' ]]; then
     echo 'Mac'
+    npm run compile && electron-builder --mac
 elif [[ $AGENT_OS == 'Linux' ]]; then
     echo 'Linux'
+    npm run compile && electron-builder --linux
 else
     echo 'unknown OS'
     exit 255
+fi
+
+if [[ $BUILD_REASON == 'PullRequest' ]]; then
+    echo 'Pull Request'
+else
+    python scripts/post_build.py
+    cat dist/*.sha256
 fi
