da6a85fdda54ca0a9eb45080fa3d14c5247d46bb
Peter Hicks <petong@jah.net>
2019-03-04 11:31:55 -0800

    more debug info

--- scripts/azure-electron-build.sh
+++ scripts/azure-electron-build.sh
@@ -2,3 +2,20 @@
 set -euxo pipefail
 
 env
+
+which python
+which python3
+which pip
+which pip3
+python --version
+python -c 'import requests'
+
+if [[ $OS == 'Windows_NT' ]]; then
+    echo 'Windows'
+elif [[ $OS == 'Darwin' ]]; then
+    echo 'Mac'
+elif [[ $OS == 'Linux' ]]; then
+    echo 'Linux'
+else
+    echo 'unknown OS'
+fi
