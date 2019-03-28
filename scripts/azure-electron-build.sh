fbd9f2df779aa8244a3a0aefca383a083097e6a7
Peter Hicks <petong@jah.net>
2019-03-28 09:35:39 -0700

    make sure windows has node 10.15

--- scripts/azure-electron-build.sh
+++ scripts/azure-electron-build.sh
@@ -20,6 +20,8 @@ esac
 
 if [[ $AGENT_OS == 'Windows_NT' ]]; then
     echo 'Windows'
+    which node
+    node --version
     #npm install --global --production windows-build-tools --vs2017
     npm install sqlite3 --build-from-source --runtime=node-webkit --target_arch=x64 --target=0.31.4 --msvs_version=2017
     npm install
