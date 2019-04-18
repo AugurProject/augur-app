6c951316bbd24630a9bbbc3abc895d376a9b3324
Peter Hicks <petong@jah.net>
2019-04-18 14:15:08 -0700

    move to azure pipelines

---
+++ scripts/azure-electron-build.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+set -euo pipefail
+
+python --version
+
+pip install requests
+
+python -c 'import requests'
+
+case $BUILD_SOURCEBRANCH in
+    *master|*release*)
+        ELECTRON_PUBLISH=always
+        ;;
+    *)
+        ELECTRON_PUBLISH=never
+        ;;
+esac
+
+if [[ $AGENT_OS == 'Windows_NT' ]]; then
+    echo 'Windows'
+    which node
+    which npx
+    find . -name electron-builder
+    export npm_config_build_from_source=false
+    npm install
+    export NODE_ENV=production
+    npm run make-win -- --publish $ELECTRON_PUBLISH
+elif [[ $AGENT_OS == 'Darwin' ]]; then
+    echo 'Mac'
+    npm install
+    npm run compile
+    NODE_ENV=production npx electron-builder --mac --publish $ELECTRON_PUBLISH
+elif [[ $AGENT_OS == 'Linux' ]]; then
+    echo 'Linux'
+    npm install
+    npm run compile
+    NODE_ENV=production npx electron-builder --linux --publish $ELECTRON_PUBLISH
+else
+    echo 'unknown OS'
+    exit 255
+fi
+
+if [[ $ELECTRON_PUBLISH == 'always' ]]; then
+    if [[ $AGENT_OS == 'Windows_NT' ]]; then
+        python scripts/post_build.py
+    else
+        python scripts\post_build.py
+    fi
+    cat dist/*.sha256
+else
+    echo 'Not generating sha256 files'
+fi
+
+#    npm install --quiet
+#    NODE_ENV=production npm run make-linux
